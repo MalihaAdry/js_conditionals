8eccc11b69b91bc6aa1149ad4815b146f5b3d8d0
MalihaAdry <maliha.adry.official@gmail.com>
2026-05-22 18:24:04 +0600

    done with comparison types

---
+++ comparison.js
@@ -0,0 +1,34 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(15 >= 10);//boro or equal 2tar 1ta hoilei output:true
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(10 <= 10);
+// console.log(100 <= 10);
+
+// console.log(10 == 10);
+// console.log(10 == 10.1);
+
+// -----------------------------
+var peyaraPrice = 40;
+var applePrice = 250;
+// console.log(peyaraPrice > applePrice);
+// console.log(applePrice < peyaraPrice);
+
+
+/**
+ * ==
+ * ===
+ * 
+ */
+
+// console.log(10 == 10);
+// console.log(10 == '10');//== a only value dekhe compare kore. data r type dekhe na
+// console.log(10 === '10');// === value and typr 2tai dekhe.tai false
+// console.log(120 === 'tomato');
+
+//-----------------
+console.log(10 != '10'); //!= thakle only man check korbe
+console.log(10 !== '10');// !== man + type 2tai dekhbe 
