315c510dbf04de6d76658b147e3b22c6b2d7dbc2
MalihaAdry <maliha.adry.official@gmail.com>
2026-05-22 17:43:17 +0600

    first commit

---
+++ comparison_types.js
@@ -0,0 +1,14 @@
+/**
+ * COMPARISON
+ * 1. bigger: >
+ * 2. less: <
+ * 3. equal: ==
+ * 4. grater than or equal: >=
+ * 5. less than or equals: <= 
+ * 6. not equal: ! =
+ * 
+ * 
+ * ---------------
+ * 7. and: &&
+ * 8. or: !! 
+ */
