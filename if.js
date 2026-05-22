f19d4dabe39613dc155c24f16742efe0ff68538a
MalihaAdry <maliha.adry.official@gmail.com>
2026-05-22 18:42:06 +0600

    if else condition complete

---
+++ if.js
@@ -0,0 +1,52 @@
+/**
+ * Tui jodi amake khawash taile ami ashbo
+ * 
+ * 
+ * -----------------
+ * if you buy food for me i will come
+ * 
+ * 
+ * --------------
+ * if biryani price less than 300 taka
+ *  will buy
+ * 
+ * 
+ * -----------------STRUCTURE-------------
+ * if (condition){
+ * 
+ * }
+ * 
+ * --------------
+ * if(condition){
+ * 
+ * }
+ * 
+ */
+if(13<10){
+    console.log('I have a smaller value');
+}
+
+var biryaniPrice = 250;
+
+if (biryaniPrice < 300){
+    console.log('mama give me biryani')
+}
+
+/**
+ * if(condition){
+ *      execute code if the condition above is true
+ * }
+ * else {
+ *      execute if the condition is false
+ * }
+ */
+
+var weight = 20;
+
+if(weight <= 20){
+    console.log('I will carry it by myself');
+
+}
+else{
+    console.log('I will rent a rickshaw');
+}
